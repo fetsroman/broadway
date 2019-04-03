99541dcc2016cdeca9977e9fdbf00ae8f34e3e40
fetsroman <fets.roman@gmail.com>
2019-04-03 19:39:16 +0300

    Add Paperclip and Change View Files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -79,6 +79,6 @@ class PlaysController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def play_params
-      params.require(:play).permit(:title, :description, :director, :category_id)
+      params.require(:play).permit(:title, :description, :director, :category_id, :play_img)
     end
 end
